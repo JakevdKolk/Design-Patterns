667deb3d865d47e4a3a1a46f5ab78a6bb189ded0
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-09-18 14:37:57 +0200

    added decorator

---
+++ 003 - DecoratorPattern/DecoratorPattern/Beverages/Beverage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern.Beverages
+{
+    enum Size
+    {
+        TALL,
+        GRANDE,
+        VENDI
+    }
+    internal abstract class Beverage
+    {
+        public Size Size { get { return size; } set { size = value; } }
+        private Size size = Size.TALL;
+
+        protected string description = "Unknown";
+        protected Beverage baseBeverage = null;
+        
+
+        public virtual string GetDescription()
+        {
+            return description;
+        }
+
+        public double GetSizeCost()
+        {
+            var mult = Size switch
+            {
+                Size.TALL => 1.0,
+                Size.VENDI => 1.2,
+                Size.GRANDE => 1.4,
+                _ => 1.0
+            };
+
+            return cost() * mult;
+        }
+
+        public abstract double cost();
+    }
+}
