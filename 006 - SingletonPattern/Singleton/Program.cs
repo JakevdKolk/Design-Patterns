234af07690043df4989ca3637c47687d6aee6265
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-10-02 13:10:19 +0200

    fixed singleton

---
+++ 006 - SingletonPattern/Singleton/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler chocolate = ChocolateBoiler.GetInstance();
+            ChocolateBoiler chocolate2 = ChocolateBoiler.GetInstance();
+
+            chocolate.fill();
+            chocolate.boil();
+            chocolate.drain();
+            Console.WriteLine(chocolate);
+
+            Console.WriteLine(chocolate2);
+        }
+    }
+}
